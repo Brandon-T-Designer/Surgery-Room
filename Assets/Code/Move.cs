45a7a48fc199f4879623bf52903aee4bb98dea3a
Brandon-T-Designer <brandontemail@gmail.com>
2024-07-25 15:36:49 -0700

    Revert "Merge branch 'Dev' into Brandon"
    This reverts commit ef5765471693583303e5e53b7c7e65af2f614e57, reversing
    changes made to 061a65a8f5ce4dbc0e9845ea9bfb85f81e9eaae0.

--- Assets/Code/Move.cs
+++ Assets/Code/Move.cs
@@ -7,8 +7,6 @@ public class Move : MonoBehaviour
     public float moveSpeed;
     float vertical, horizontal;
     Rigidbody2D myRigidbody2D;
-    public Animator PlayerAnimator;
-    public bool movingRight;
 
     // Start is called before the first frame update
     void Start()
@@ -24,19 +22,13 @@ public class Move : MonoBehaviour
 
     void MoveBall()
     {
-        horizontal = Input.GetAxisRaw("Horizontal")*moveSpeed;
-        if(Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            transform.localScale = new Vector2(-.85f, .85f);
-        }
-        else if (Input.GetKeyUp(KeyCode.RightArrow))
-        {
-            transform.localScale = new Vector2(.85f, .85f);
-        }
-        PlayerAnimator.SetFloat("Horizontal", Mathf.Abs(horizontal));
-              
-        vertical = Input.GetAxisRaw("Vertical")*moveSpeed;
-        PlayerAnimator.SetFloat("Vertical", Mathf.Abs(vertical));
+        horizontal = Input.GetAxis("Horizontal");
+        //Insert Animation Code
+
+        vertical = Input.GetAxis("Vertical");
+        //Insert Animation Code
+
+        //Debug.Log(horizontal);
         myRigidbody2D.velocity = new Vector2(horizontal*moveSpeed, vertical * moveSpeed);
     }
 }
