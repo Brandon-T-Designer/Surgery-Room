75a97aff28eff57c015dfd8510e6d33f6648e79c
Brandon-T-Designer <brandontemail@gmail.com>
2024-07-26 16:57:38 -0700

    Gilead Changes 1000
    Gilead Changes 1000

--- Assets/Code/OpenPopup.cs
+++ Assets/Code/OpenPopup.cs
@@ -21,6 +21,16 @@ public class OpenPopup : MonoBehaviour
             Time.timeScale = 0;
             popupWindow.SetActive(true);
             IsThisPopUpOpen = true;
+
+            //Gilead Code
+            InventoryManager.instance.itemSlotsParentWhenInDrugCabinet.gameObject.SetActive(true);
+            for (int i = 0; i < InventoryManager.instance.itemSlotsParentWhenNotInDrugCabinet.childCount; i ++)
+            {
+                Transform itemSlotTrs = InventoryManager.instance.itemSlotsParentWhenNotInDrugCabinet.GetChild(i);
+                itemSlotTrs.SetParent(InventoryManager.instance.itemSlotsParentWhenInDrugCabinet);
+                itemSlotTrs.localScale = Vector3.one;
+                i --;
+            }
         }
     }
 
@@ -42,5 +52,15 @@ public class OpenPopup : MonoBehaviour
             popupWindow.SetActive(false);
             IsThisPopUpOpen = false;
             Time.timeScale = 1;
+
+        //Gilead Code
+        InventoryManager.instance.itemSlotsParentWhenInDrugCabinet.gameObject.SetActive(false);
+        for (int i = 0; i < InventoryManager.instance.itemSlotsParentWhenInDrugCabinet.childCount; i ++)
+        {
+            Transform itemSlotTrs = InventoryManager.instance.itemSlotsParentWhenInDrugCabinet.GetChild(i);
+            itemSlotTrs.SetParent(InventoryManager.instance.itemSlotsParentWhenNotInDrugCabinet);
+            itemSlotTrs.localScale = Vector3.one;
+            i --;
+        }
     }
 }
