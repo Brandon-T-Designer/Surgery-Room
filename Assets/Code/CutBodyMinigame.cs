761f2dea791bf7b41d8d2e50619f01ae5c024c06
Gilead Cosman <gileadcosman@gmail.com>
2024-08-05 15:03:36 -0700

    Mostly done with cut body minigame

--- Assets/Code/CutBodyMinigame.cs
+++ Assets/Code/CutBodyMinigame.cs
@@ -6,64 +6,90 @@ using UnityEngine.SceneManagement;
 
 public class CutBodyMinigame : MonoBehaviour
 {
-	public float duration;
+	public float cauterizeDuration;
 	public GameObject gameOverScreen;
 	public GameObject mustCauterizePanel;
-	public float circleCollidersSeparation;
+	public LineRenderer lineRenderer;
+	public float cutPointSeparation;
 	public float circleColliderRadius;
+	List<Vector2> cutPoints = new List<Vector2>();
+	float distanceToNextCutPoint;
+	float previousMousePosition;
+	float cauterizeTimer;
 	Move_Body body;
-	float timer;
 
 	void OnEnable ()
 	{
 		Time.timeScale = 0;
+		cutPoints.Clear();
+		lineRenderer.startWidth = circleColliderRadius;
+		lineRenderer.endWidth = circleColliderRadius;
 	}
 
 	void Update ()
 	{
+		Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
 		if (Mouse.current.leftButton.wasPressedThisFrame)
 		{
 			if (!mustCauterizePanel.activeSelf)
 			{
-				Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
 				if (!body.cutStartZoneBoxCollider.bounds.Contains(mousePosition))
-				{
-					mustCauterizePanel.SetActive(true);
-					
-				}
-			}
-			else
-			{
-
-			}
-		}
-		else if (Mouse.current.rightButton.wasPressedThisFrame)
-		{
-			if (!mustCauterizePanel.activeSelf)
-			{
-				Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
-				if (body.cutEndZoneBoxCollider.bounds.Contains(mousePosition))
-				{
-					Time.timeScale = 1;
-					Destroy(body.gameObject);
-				}
-				else
-				{
-					mustCauterizePanel.SetActive(true);
-				}
+					StartCauterize ();
 			}
 			else
 			{
 				
 			}
 		}
-		timer += Time.deltaTime;
-		if (timer >= duration)
+		else if (Mouse.current.rightButton.wasPressedThisFrame)
 		{
-			gameOverScreen.SetActive(true);
-			mustCauterizePanel.SetActive(false);
-			enabled = false;
+			if (!mustCauterizePanel.activeSelf)
+			{
+				if (body.cutEndZoneBoxCollider.bounds.Contains(mousePosition))
+				{
+					Time.timeScale = 1;
+					Destroy(body.gameObject);
+					enabled = false;
+				}
+				else
+					StartCauterize ();
+			}
+			else
+			{
+				
+			}
 		}
+		if (mustCauterizePanel.activeSelf)
+		{
+			cauterizeTimer += Time.deltaTime;
+			if (cauterizeTimer >= cauterizeDuration)
+			{
+				gameOverScreen.SetActive(true);
+				mustCauterizePanel.SetActive(false);
+				enabled = false;
+			}
+		}
+		else if (Mouse.current.leftButton.isPressed)
+		{
+			distanceToNextCutPoint -= (mousePosition - previousMousePosition).magnitude;
+			if (distanceToNextCutPoint <= 0)
+			{
+				while (distanceToNextCutPoint <= 0)
+				{
+					Vector2 cutPoint = previousMousePosition + (mousePosition - previousMousePosition).normalized * cutPointSeparation;
+					cutPoints.Add(cutPoint);
+					distanceToNextCutPoint += cutPointSeparation;
+				}
+				lineRenderer.SetPositions(cutPoints.ToArray());
+			}
+		}
+		previousMousePosition = mousePosition;
+	}
+
+	void StartCauterize ()
+	{
+		mustCauterizePanel.SetActive(true);
+		cauterizeTimer = 0;
 	}
 
 	public void ResetLevel ()
